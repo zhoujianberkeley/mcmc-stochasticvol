b75d4446488de2148cc3e80b8ac42c19e1eea70e
Jian <zhoujian@berkeley.edu>
2021-01-30 17:00:25 +0800

    add AR1 and GRW

--- proposal_assignment.py
+++ proposal_assignment.py
@@ -243,19 +243,14 @@ plt.show()
 
 # ### Baseline Model: Stochastic Volatility
 
-# In[15]:
-
-
-data
-
-
 # In[24]:
 
 def make_baseline_model(data):
     with pm.Model() as model:
         simga_ita2 = pm.InverseGamma("simga_ita2", alpha=2.5, beta=0.05)
         phi = pm.Beta("phi", alpha=20, beta=1.5)
-        log_vol = pm.AR1("log_vol", k=phi, tau_e=1/simga_ita2, shape=len(data))
+        log_vol = pm.GaussianRandomWalk('log_vol', mu=0, sigma=np.sqrt(simga_ita2), shape=len(data))
+        # log_vol = pm.AR1("log_vol", k=phi, tau_e=1/simga_ita2, shape=len(data))
         returns = pm.Normal("returns", mu=0, sigma=np.exp(log_vol/2), observed=data["SP500"])
     return model
 
